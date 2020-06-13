1ccd12ce7c02440bcae40b130c976e43017a33de
Rees Dart <thedominicparker@gmail.com>
2020-06-12 23:37:35 -0700

    first prompt loop completed, resulting in user inputting a valid number.

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// need to creat a bunch of arrays that represent each of the different type of
-// input categories (special characters, lowercase, uppercase, etc,..)
+// need to create a bunch of arrays that represent each of the different type of characters,numbers, letters, etc.
 // you can use the .split(), or lowercase method, or uppercase method, etc
+
+// (special characters)
+var specChar = ["!","#","$","%","&","(",")","*","+","-","/",":",";","<","=",">","?","@","^","_","~"];
+// (numbers)
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+// (lowercase)
+var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// (uppercase)
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 var possibleCharacters=[];
 
-//TODO: Create generatePassword
 
-  //TODO: Prompt the user for password length
-    //TODO: store password length in the variable
+// Create generatePassword
+  // prompt user for password strength
+var passwordLengthInput = prompt("How many characters do you want in your password? Enter a number between 8 - 128.");
+// console.log(passwordLength);
+// convert user input passwordLength from a string to an integer
+passwordLengthInput = parseInt(passwordLengthInput);
+// console.log(passwordLength);
+// if passwordLengthinput is a number between 8-128, store passwordLengthInput as variable passwordLength
+while (passwordLengthInput < 8 || passwordLengthInput > 128) {
+  passwordLengthInput = prompt("Please enter a valid number between 8 - 128");
+  if (passwordLengthInput >= 8 && passwordLengthInput <= 128) {
+    var specCharInput = confirm("Would you like special characters in your generated password?");
+  }
+}
+  
+  
+// }
+
   // confirm whether user wants special characters
     // store in includeSpecial (booleans)
   // confirm if the user wants numbers
@@ -28,7 +51,7 @@ var possibleCharacters=[];
 
 var passwordStr=""
 
-return passwordStr
+// return passwordStr
 
 // Write password to the password input
 function writePassword() {
