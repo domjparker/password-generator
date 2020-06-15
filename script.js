9e199e0c22b1384926d069fb3f5c76b437f3ddbe
Rees Dart <thedominicparker@gmail.com>
2020-06-15 01:01:08 -0700

    redid certain variable names for consistency. Have a bug coming up that will need to be fixed, with certain password results coming back 'undefined'.

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function generatePassword() {
   // (special characters)
   var specChar = ["!","#","$","%","&","<",">","?","@","~"];
   // (numbers)
-  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var numChar = ["0","1","2","3","4","5","6","7","8","9"];
   // (lowercase)
-  var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var lowerChar =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   // (uppercase)
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-  var possibleCharacters=[];
+  var possibleChar=[];
 
 
   // Create generatePassword
@@ -35,41 +35,43 @@ function generatePassword() {
   // console.log(specCharInput)
   // confirm whether user wants special characters in password
   if (specCharInput == true) {
-    possibleCharacters = possibleCharacters.concat(specChar);
+    possibleChar = possibleChar.concat(specChar);
   }
   // console.log(possibleCharacters);
   // confirm whether user wants numbers in password
   var numInput = confirm("Would you like numbers included your generated password?");
   if (numInput == true) {
-    possibleCharacters = possibleCharacters.concat(numbers);
+    possibleChar = possibleChar.concat(numChar);
   }
   // console.log(possibleCharacters);
   // confirm whether user wants lower case letters in password
   var lowerCaseInput = confirm("Would you like lower case letters included in your generated password?");
   if (lowerCaseInput == true) {
-    possibleCharacters = possibleCharacters.concat(lowerCase);
+    possibleChar = possibleChar.concat(lowerChar);
   }
   // console.log(possibleCharacters);
   // confirm whether user wants upper case letters in password
   var upperCaseInput = confirm("Would you like upper case letters included in your generated password?");
   if (upperCaseInput == true) {
-    possibleCharacters = possibleCharacters.concat(upperCase);
+    possibleChar = possibleChar.concat(upperChar);
   }
-  console.log(possibleCharacters);
+  // console.log(possibleChar);
+
+  if (specCharInput == false && numInput == false && lowerCaseInput == false & upperCaseInput == false)
 
   // when possibleCharacters array completed, build a password
 
   var passwordStr=""
 
   for (var i=0; i<passwordLengthInput; i++) {
-    var randomNum = Math.floor(Math.random() * possibleCharacters.length+1);
+    var randomNum = Math.floor(Math.random() * (possibleChar.length+1));
     // console.log(randomNum)
-    var randomCharacter = possibleCharacters[randomNum]
+    var randomCharacter = possibleChar[randomNum]
     // console.log(randomCharacter)
     passwordStr += randomCharacter
     // console.log(passwordStr)
   }
-
+  console.log(passwordStr)
   return passwordStr
 }
 
